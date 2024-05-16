6ebba6ae80078cf8a0752f52e6c63cbff78c93cc
tteia <tteia.dl@gmail.com>
2024-05-16 17:06:53 +0900

    mariaDB_syntax

--- 1.DDL.sql
+++ 1.DDL.sql
@@ -35,3 +35,30 @@ create table posts(id INT PRIMARY KEY, title VARCHAR(255), content VARCHAR(255),
 -- 테이블 index 조회
 show index from author;
 show index from posts;
+
+-- ALTER문 : 테이블의 구조를 변경
+-- 테이블 이름 변경
+alter table posts rename post;
+-- 테이블 컬럼 추가
+-- 수정은 add 대신 modify
+alter table author add column test1 VARCHAR(50);
+-- 테이블 컬럼 삭제
+alter table author drop column test1;
+-- 테이블 컬럼명 변경
+alter table post change column content contents VARCHAR(255);
+-- 테이블 컬럼 타입과 제약 조건 변경
+-- 덮어쓰기와 같은 개념이기 때문에 기존에 있던 값도 빼먹지 말고 적어주기!
+alter table author modify column email VARCHAR(255) not null;
+
+
+--실습 : author 테이블에 address 컬럼 추가. 
+alter table author add column address VARCHAR(255);
+
+--실습 : post 테이블에 title은 not null 제약 조건 추가, contents는 3000자로 변경.
+--,를 통해 한 줄로 작성할 수 있다!
+alter table post modify column title VARCHAR(255) not null, modify column contents VARCHAR(3000);
+
+-- 테이블 삭제
+-- 복구 => show create  table post; 로 조회 후 복사, 그대로 붙여넣기 하면 복구 완료!
+--  CREATE TABLE `post` ( `id` int(11) NOT NULL, `title` varchar(255) NOT NULL, `contents` varchar(3000) DEFAULT NULL, `author_id` int(11) DEFAULT NULL, PRIMARY KEY (`id`), KEY `author_id` (`author_id`), CONSTRAINT `post_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `author` (`id`));
+drop 테이블명;
--- 2.DML.sql
+++ 2.DML.sql
@@ -11,5 +11,44 @@ insert into posts(id, title, content, author_id) values(1, 'hello', 'hello world
 
 -- 테이블 제약 조건 조회
 -- 내가 현재 use하고 있는 스키마에서는 select * from 테이블명;
--- use 하고 있지 않다면? selecet * from board.author; (스키마 명.table);
+-- use 하고 있지 않다면? select * from board.author; (스키마 명.table);
 select * from information_schema.key_column_usage where table_name = 'posts';
+
+-- update 테이블명 set 컬럼명1=데이터1, 컬럼명2=데이터2, ...  where id = 1;
+-- where문이 빠질 경우 모든 데이터에 update 가 적용됨.
+update author set email='abc@test.com' where id=2;
+
+
+-- delete from 테이블명 where
+delete from author where id = 3;
+
+-- SELECT의 다양한 조회 방법
+select * from author;
+select * from author where id = 1;
+select * from author where id > 2 and name = bambi;
+
+-- 특정 컬럼만 조회할 때
+select name, email from author where id = 3;
+
+-- 중복 제거하고 조회
+select distinct title from post;
+
+-- 정렬 : order by, 데이터의 출력결과를 특정 기준으로 정렬
+-- 아무런 정렬 조건없이 조회할 경우에는 pk 기준 오름차순으로 조회.
+-- asc : 오름차순, desc : 내림차순
+select * from author order by name asc;
+
+-- 멀티 order by : 여러 컬럼으로 정렬. 먼저 쓴 컬럼 우선 정렬, 그 다음 정렬 옵션 적용.
+-- asc : 오름차순, desc : 내림차순
+select * from post order by contents;
+select * from post order by title, id desc; -- 중복일 경우에 id desc 하겠다!
+
+-- limit number : 특정 숫자로 결과값 개수 제한
+select * from author order by id desc limit 1;
+
+-- alias(별칭)을 이용한 select : as 키워드 사용
+select name as 이름, email as 이메일 from author;
+select a.name as 이름, a.email as 이메일 from author as a; -- 테이블이 여러 개일 때 사용
+
+-- null을 조회 조건으로
+select * from post where author_id is null; -- 비어있는 것만 조회 null / 아닌 것 not null
