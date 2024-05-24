531c80c30331fd559eedac6a5b070cfd7d9826fb
tteia <tteia.dl@gmail.com>
2024-05-24 17:48:01 +0900

    mariadb_syntax

--- 12.user_view_procedure.sql
+++ 12.user_view_procedure.sql
@@ -50,3 +50,129 @@ END
 
 -- 프로시저 호출
 call test_procedure();
+
+-- 프로시저 삭제
+drop procedure test_procedure;
+
+-- 게시글 목록 조회 프로시저 생성
+DELIMITER //
+CREATE PROCEDURE 게시글목록조회()
+BEGIN 
+    select * from post;
+END
+// DELIMITER ;
+
+call 게시글목록조회();
+
+-- 게시글 단건 조회
+DELIMITER //
+CREATE PROCEDURE 게시글단건조회(in postID int)
+BEGIN 
+    select * from post where id = postID;
+END
+// DELIMITER ;
+
+call 게시글단건조회();
+
+-- 저자id로 조회
+DELIMITER //
+CREATE PROCEDURE 게시글단건조회(in 저자id int, in 제목 varchar(255))
+BEGIN 
+    select * from post where author_id = 저자id and title = 제목;
+END
+// DELIMITER ;
+
+call 게시글단건조회(4, 'hello');
+
+-- 글쓰기
+DELIMITER //
+CREATE PROCEDURE 글쓰기(in 제목 varchar(255), in 내용 varchar(3000), in 저자id int)
+BEGIN 
+    insert into post(title, contents, author_id) values(title = 제목, contents = 내용, author_id = 저자id);
+END
+// DELIMITER ;
+
+call 글쓰기('hi', 'hello', 1);
+-- 왜 안될까..?
+insert into post(title, contents, author_id) values(제목, 내용, 저자id);
+-- 위 코드로 작성해야하는데 잘못 지정했다 ^^;;
+DELIMITER //
+CREATE PROCEDURE 글쓰기(in 제목 varchar(255), in 내용 varchar(3000), in 저자id int)
+BEGIN 
+    insert into post(title, contents, author_id) values(제목, 내용, 저자id);
+END
+// DELIMITER ;
+
+-- 글쓰기 : title, contents, email
+DELIMITER //
+CREATE PROCEDURE 게시글생성2(in 이메일 varchar(255), in 제목 varchar(255), in 내용 varchar(3000))
+BEGIN
+	declare authorId int;     
+	select id into authorId from author where email = 이메일;     
+	insert into post(title, contents, author_id) values(제목, 내용, authorId); 
+END 
+// DELIMITER ;  
+	
+call 게시글생성2('hong1@naver.com','hello', 'world');
+
+-- sql에서 문자열 합치는 concat('hello', 'world');
+-- 글 상세조회 : input 값이 postId
+-- title, contents, '홍길동' + '님'
+
+DELIMITER //
+CREATE PROCEDURE 상세조회(in postId int)
+BEGIN
+    declare authorName varchar(255);
+    SELECT name into authorName from author where id = (select author_id from post where id = postId);
+    set authorName = concat(authorName, '님');
+    SELECT title, contents, authorName from post where id = postId;
+END
+// DELIMITER ;
+
+-- join 활용해보기
+
+
+
+-- 등급 조회 >> IF문 활용
+-- 글을 100개 이상 쓴 사용자는 고수입니다. 출력
+-- 글을 10개 이상, 100개 미만 중수입니다.
+-- 그 외 초보입니다.
+DELIMITER //
+CREATE PROCEDURE 등급조회(in emailInput varchar(255))
+BEGIN
+    declare authorID int;
+    declare count int;
+    select id into authorId from author where email = emailInput;
+    select count(*) into count from post where author_id = authorId;
+    IF count >= 100 then
+        select '고수입니다.';
+    ELSEIF count >= 10 and count < 100 then
+        select '중수입니다.';
+    ELSE
+        select '초보입니다.';
+    END IF;
+END
+// DELIMITER ;
+
+-- 반복을 통해 post 대량 생성
+-- 사용자가 입력한 반복 횟수에 따라 글이 도배되는데, title은 '안녕하세요'
+DELIMITER //
+CREATE PROCEDURE 글도배(in ab int)
+BEGIN
+    declare count int default 0;
+    WHILE count < ab DO
+        INSERT INTO post(title) values('안녕하세요');
+        SET count = count + 1;
+    END WHILE;
+END
+// DELIMITER ;
+
+describe post;
+-- NULL > YES : 비어있어도 됨
+-- NULL > NO : 비어있으면 안됨!
+
+-- 프로시저 생성문 조회
+show create procedure 프로시저명;
+
+-- 프로시저 권한 부여
+grant excute on board.글도배 to 'test1'@'localhost';
---
+++ 13.redis.sh
@@ -0,0 +1,44 @@
+brew services start redis
+
+
+# 레디스 접속
+# cli : commandline interface
+redis-cli
+
+# redis는 0~15번까지의 database 구성
+# 데이터베이스 선택
+select 번호 # default = 0
+
+# 데이터 베이스 내 모든 키 조회
+keys *
+# 데이터 베이스 내 모든 value 조회
+get value
+# get을 통해 value 값 얻기
+get test_key1
+
+# 일반 string 자료구조
+# key:value 값 세팅
+# key값은 중복되면 안된다! > 중복되면 덮어쓰여지는 것이 기본. : 맵 저장소에서 key 값은 유일하게 관리가 되므로.
+SET key(키) value(값) # key와 value에 각각 값을 넣는 것!
+set test_key1 test_value1
+set user:email:1 hong@test.com # 'user:email:1' 이 문자열! user_email_1로 써도 동일하다.
+# > set user:email:1 로 조회
+# nx : not exist
+set user:email:2 hong1@test.com nx # user:email:1 에 값이 nx 할 때만 value를 넣겠다!
+# ex : 만료시간 - 초 단위 > ttl(time to live) 
+set user:email:2 hong2@test.com ex 20
+
+# 특정 key 삭제
+del user:email:1
+# 현재 데이터베이스의 모든 key 값 삭제
+flushdb
+
+# 좋아요 기능 구현
+set likes:posting:1 0 # posting 게시글 1번의 좋아요가 0
+incr likes:posting:1 # 특정 key 값의 value를 1만큼 증가시키겠다. # 0 자체는 문자열!
+decr likes:posting:1
+
+# 재고 기능 구현
+set product:1:stock 100
+decr product:1:stock
+get product:1:stock
